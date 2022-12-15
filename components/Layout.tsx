2fbc91ad688e06d244ffc0036e029bfc1284ba71
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-15 12:12:16 -0800

    Built Layout

---
+++ components/Layout.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react';
+import styles from '../styles/Layout.module.css';
+import Meta from "../components/Meta"
+import Nav from "../components/Nav"
+import Footer from "../components/Footer"
+
+interface Props {
+  children?: ReactNode;
+  // any props that come into the component
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+    <Meta/>
+    <Nav/>
+    <div className={styles.container}>
+      <main className={styles.main}>{children}</main>
+    </div>
+    <Footer/>
+    </>
+  );
+};
+
+export default Layout;
