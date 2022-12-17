2159e6b1cfe1909f959f0da74769efdeb46088a9
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-16 23:34:41 -0800

    Updated main index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import Header from '../components/Header';
+import About from '../components/About';
+import Skills from '../components/Skills/Skills';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
 
 export default function Home() {
   return (
-    // <div className={styles.container}>
     <div>
       <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
