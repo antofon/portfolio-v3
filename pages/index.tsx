6966ef1460a5108db0cccbf8d1b75ec7e80316eb
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-17 21:29:40 -0800

    Updated Projects directory import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Header from '../components/Header';
 import About from '../components/About';
 import Skills from '../components/Skills/Skills';
-import Projects from '../components/Projects';
+import Projects from '../components/Projects/Projects';
 import Contact from '../components/Contact';
 
 export default function Home() {
