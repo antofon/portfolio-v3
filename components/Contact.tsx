328e559c346aeba5018b604c06f58a27131a78ca
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-19 12:17:40 -0800

    Finished layout and responsiveness of Contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,17 +4,16 @@ import contactImg from '../public/assets/images/about/joshua-woroniecki-lzh3hPtJ
 import { FaLinkedinIn, FaGithub, FaCodepen } from 'react-icons/fa';
 
 const Contact = () => {
+  const handleSubmit = () => {
+    console.log('submit clicked');
+  };
 
-  const handleSubmit = ()=> {
-    console.log("submit clicked");
-  }
-  
   return (
     <div id="contact" className={contactStyles.container}>
       <h2 className={contactStyles.sectionTitle}>CONTACT</h2>
       <p className={contactStyles.sectionDescription}>Get In Touch</p>
 
-      <div className={contactStyles.grid}>
+      <div className={contactStyles.wrapper}>
         <div className={contactStyles.connectContainer}>
           <Image
             src={contactImg}
@@ -24,55 +23,100 @@ const Contact = () => {
             className={contactStyles.img}
           ></Image>
 
-          <p>Anwana Ntofon</p>
-          <p>Front-End Web Developer</p>
-          <p>
+          <p className={contactStyles.name}>Anwana Ntofon</p>
+          <div className={contactStyles.bodyText}>
+          <p className={contactStyles.title}>Front-End Web Developer</p>
+          <p className={contactStyles.description}>
             I am available for freelance or full-time positions. Contact me and{' '}
             {"let's"} talk
           </p>
-          <p className={contactStyles.connectText}>Connect with me</p>
-          <div className={contactStyles.socials}>
-            <div>
-              <a
-                href="https://www.linkedin.com/in/anwanantofon/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={contactStyles.icon}
-              >
-                <FaLinkedinIn />
-              </a>
-            </div>
-            <div>
-              <a
-                href="https://github.com/antofon"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={contactStyles.icon}
-              >
-                <FaGithub />
-              </a>
-            </div>
-            <div>
-              <a
-                href="https://codepen.io/antofon"
-                rel="noopener noreferrer"
-                className={contactStyles.icon}
-              >
-                <FaCodepen />
-              </a>
+          </div>
+
+          <div className={contactStyles.socialsContainer}>
+            <p className={contactStyles.connectText}>Connect with me</p>
+            <div className={contactStyles.socials}>
+              <div>
+                <a
+                  href="https://www.linkedin.com/in/anwanantofon/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={contactStyles.icon}
+                >
+                  <FaLinkedinIn />
+                </a>
+              </div>
+              <div>
+                <a
+                  href="https://github.com/antofon"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={contactStyles.icon}
+                >
+                  <FaGithub />
+                </a>
+              </div>
+              <div>
+                <a
+                  href="https://codepen.io/antofon"
+                  rel="noopener noreferrer"
+                  className={contactStyles.icon}
+                >
+                  <FaCodepen />
+                </a>
+              </div>
             </div>
           </div>
         </div>
 
         <div className={contactStyles.formContainer}>
-          <form action="/send-data-here" method="post">
-            <label htmlFor="roll">Roll Number</label>
-            <input type="text" id="roll" name="roll" required />
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" required />
-            <button type="submit">Submit</button>
+          <form className={contactStyles.form}>
+            <label htmlFor="name" className={contactStyles.formLabel}>
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              className={contactStyles.formInput}
+            />
+            <label htmlFor="email" className={`${contactStyles.formLabel} ${contactStyles.labelMarginTop}`}>
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              className={contactStyles.formInput}
+            />
+            <label htmlFor="subject" className={`${contactStyles.formLabel} ${contactStyles.labelMarginTop}`}>
+              Subject
+            </label>
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              className={contactStyles.formInput}
+            />
+            <label htmlFor="message" className={`${contactStyles.formLabel} ${contactStyles.labelMarginTop}`}>
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              cols={30}
+              rows={10}
+              className={contactStyles.formMessage}
+            ></textarea>
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className={contactStyles.button}
+            >
+              Send Message
+            </button>
           </form>
-          <button onClick={handleSubmit}/>
         </div>
       </div>
     </div>
