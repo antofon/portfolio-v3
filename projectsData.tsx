f2f79702b9d46d0cea617fdea11b2e551efa1467
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-20 19:44:44 -0800

    Added new properties to data file

--- projectsData.tsx
+++ projectsData.tsx
@@ -1,10 +1,12 @@
 export interface ProjectsProps {
   projects: Projects[];
+  projectNumber:number;
 }
 export interface Projects {
   projectsImage: string;
   projectsTitle: string;
   projectsStack: string;
+  projectsUrl: string;
 }
 
 const projectsData: Projects[] = [
@@ -13,24 +15,28 @@ const projectsData: Projects[] = [
       '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
     projectsTitle: 'Portfolio Website',
     projectsStack: 'React/CSS/Next.js',
+    projectsUrl: '/portfolio',
   },
   {
     projectsImage:
       '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
     projectsTitle: 'StemReads',
     projectsStack: 'React/Sass/Firebase',
+    projectsUrl: '/stemreads',
   },
   {
     projectsImage:
       '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
     projectsTitle: 'Wikipedia Viewer',
     projectsStack: 'HTML/CSS/JavaScript/Wikipedia API',
+    projectsUrl: '/wikipediaapi',
   },
   {
     projectsImage:
       '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
     projectsTitle: 'Twitch Streamers',
     projectsStack: 'HTML/CSS/JavaScript/Twitch API',
+    projectsUrl: '/twitchstreamers',
   },
 ];
 
