1742fdcd5c511fe24dffbfd3cf66aa1f1da13342
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-20 18:40:10 -0800

    Created env variables

---
+++ environmnt.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
+      SERVICE_ID: string;
+      TEMPLATE_ID: string;
+      PUBLIC_ID: string;
+    }
+  }
+}
+
+export {}
