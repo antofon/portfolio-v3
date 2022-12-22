357984e0f6619a67669691d08cc4c7543e7d9e13
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-21 20:51:26 -0800

    Working on Nav display new nav styling on homepage vs project page, also corrected url paths

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,10 +2,30 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import navStyles from '../styles/Nav.module.css';
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const [shadow, setShadow] = useState(false);
+  const [navBg, setNavBg] = useState('#F7F7F7');
+  const [linkColor, setLinkColor] = useState('#000000');
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (
+      router.asPath === '/portfolio' ||
+      router.asPath === '/stemreads' ||
+      router.asPath === '/wikipedia' ||
+      router.asPath === '/twitch'
+    ) {
+      setNavBg('transparent');
+      setLinkColor('#FFFFFF');
+    } else {
+      setNavBg('#F7F7F7');
+      setLinkColor('#000000');
+    }
+  }, [router]);
 
   const handleNav = (): void => {
     setNav(!nav);
@@ -24,34 +44,39 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className={shadow ? navStyles.shadow : navStyles.navbar}>
+    <nav
+      className={shadow ? navStyles.shadow : navStyles.navbar}
+      style={{ backgroundColor: `${navBg}` }}
+    >
       <div>
-        <Image
-          src="/assets/images/logo-v2.png"
-          alt="logo"
-          width="100"
-          height="100"
-        />
+        <Link href="/">
+          <Image
+            src="/assets/images/logo-v2.png"
+            alt="logo"
+            width="100"
+            height="100"
+          />
+        </Link>
       </div>
       <div>
-        <ul className={navStyles.links}>
+        <ul className={navStyles.links} style={{ color: `${linkColor}` }}>
           <Link href="/">
             <li className={navStyles.link}>Home</li>
           </Link>
 
-          <Link href="#about">
+          <Link href="/#about">
             <li className={navStyles.link}>About</li>
           </Link>
 
-          <Link href="#skills">
+          <Link href="/#skills">
             <li className={navStyles.link}>Skills</li>
           </Link>
 
-          <Link href="#projects">
+          <Link href="/#projects">
             <li className={navStyles.link}>Projects</li>
           </Link>
 
-          <Link href="#contact">
+          <Link href="/#contact">
             <li className={navStyles.link}>Contact</li>
           </Link>
         </ul>
