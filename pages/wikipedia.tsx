ab29acc78d19eab3c190818fe9f28351712f951d
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-22 20:06:32 -0800

    Created Wikipedia project page

---
+++ pages/wikipedia.tsx
@@ -0,0 +1,12 @@
+import projectPageData from '../data/projectPageData';
+import ProjectPage from '../components/ProjectPage/ProjectPage';
+
+const wikipedia = () => {
+  return (
+    <>
+      <ProjectPage projects={projectPageData} projectId={2} />
+    </>
+  );
+};
+
+export default wikipedia;
