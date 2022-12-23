c543507ac6b81a2ae5565e22a811b75bbc030483
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-23 11:04:59 -0800

    Implementated back to top

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -2,9 +2,13 @@ import Link from 'next/link';
 import { FaChevronUp } from 'react-icons/fa';
 import backToTopStyles from '../styles/BackToTop.module.css';
 
-const BackToTop = () => {
+interface BackToTop {
+  visible: string;
+}
+
+const BackToTop = ({ visible }: BackToTop) => {
   return (
-    <div className={backToTopStyles.container}>
+    <div className={backToTopStyles.container} style={{ display: visible }}>
       <Link href="/">
         <div className={backToTopStyles.chevronContainer}>
           <FaChevronUp />
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,36 @@
+import { useState, useEffect } from 'react';
 import footerStyles from '../styles/Footer.module.css';
 import BackToTop from './BackToTop';
 
 const Footer = () => {
+  const [visible, setVisible] = useState('none');
+
+  const handleScroll = () => {
+    const bottom =
+      Math.ceil(window.innerHeight + window.scrollY + 200) >=
+      document.documentElement.scrollHeight;
+
+    if (bottom) {
+      setVisible('flex');
+    } else {
+      setVisible('none');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, {
+      passive: true,
+    });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
-    // <>
-      <footer className={footerStyles.footer}>
-      {/* <BackToTop /> */}
-        <p className={footerStyles.footerText}>© 2022 by Anwana Ntofon</p>
-      </footer>
-    // {/* </> */}
+    <footer className={footerStyles.footer}>
+      <BackToTop visible={visible} />
+      <p className={footerStyles.footerText}>© 2022 by Anwana Ntofon</p>
+    </footer>
   );
 };
 
