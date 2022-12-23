243c9451fd8fd38516016d29490a60126d194830
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-22 17:26:46 -0800

    Completed Project Page Header and Body skeleton

--- components/ProjectPage/ProjectPageBody.tsx
+++ components/ProjectPage/ProjectPageBody.tsx
@@ -7,47 +7,45 @@ const ProjectPageBody = ({ projects, projectId }: ProjectPageProps) => {
   const project = projects[projectId];
   return (
     <div className={projectPageBodyStyles.projectBody}>
-      <p className={projectPageBodyStyles.bodyProjectTitle}>Project</p>
-      <h2 className={projectPageBodyStyles.bodyOverviewTitle}>Overview</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
-        nulla? Nemo possimus corporis voluptatem quae earum minima saepe quod
-        labore quisquam? Optio voluptas modi reiciendis nam sit sapiente culpa
-        vitae!
-      </p>
+      <div className={projectPageBodyStyles.projectBodyText}>
+        <p className={projectPageBodyStyles.bodyProjectTitle}>Project</p>
+        <h2 className={projectPageBodyStyles.bodyOverviewTitle}>Overview</h2>
+        <p className={projectPageBodyStyles.projectDescription}>
+          {project.projectDescription}
+        </p>
+        {/* <Link href="/#projects" className={projectPageBodyStyles.backButton}>
+          Back
+        </Link> */}
+        <div className={projectPageBodyStyles.buttonContainer}>
+          <a
+            href="https://anwanantofon.com"
+            className={projectPageBodyStyles.button}
+            rel="noopener noreferrer"
+          >
+            Demo
+          </a>
+          <a
+            href=""
+            className={`${projectPageBodyStyles.button} ${projectPageBodyStyles.marginLeft}`}
+            rel="noopener noreferrer"
+          >
+            Code
+          </a>
+        </div>
+        <div></div>
+      </div>
       <div className={projectPageBodyStyles.technologiesContainer}>
-        <p>Technologies</p>
-        <div>
-          {project.projectsStack.map((item, index) => (
+        <p className={projectPageBodyStyles.technologiesTitle}>Technologies</p>
+        <div className={projectPageBodyStyles.technologiesStackContainer}>
+          {project.projectPageStack.map((item, index) => (
             <p key={index}>
               <FaDotCircle className={projectPageBodyStyles.dotCircle} />
               <span className={projectPageBodyStyles.technology}>{item}</span>
             </p>
           ))}
         </div>
-        <Link href="/#projects" className={projectPageBodyStyles.backButton}>
-          Back
-        </Link>
       </div>
     </div>
-
-    //  <div className={projectPageBodyStyles.buttonContainer}>
-    //   <a
-    //     href="https://anwanantofon.com"
-    //     className={projectPageBodyStyles.button}
-    //     rel="noopener noreferrer"
-    //   >
-    //     Demo
-    //   </a>
-    //   <a
-    //     href=""
-    //     className={`${projectPageBodyStyles.button} ${projectPageBodyStyles.marginLeft}`}
-    //     rel="noopener noreferrer"
-    //   >
-    //     Code
-    //   </a>
-    // </div>
-    //  <div>
   );
 };
 
--- components/ProjectPage/ProjectPageHeader.tsx
+++ components/ProjectPage/ProjectPageHeader.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 import { ProjectPageProps } from '../../projectPageData';
-import projectPageHeaderStyles from "../../styles/ProjectPage/ProjectPageHeader.module.css"
+import projectPageHeaderStyles from '../../styles/ProjectPage/ProjectPageHeader.module.css';
+import projectImg from '../../public/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg';
 
 const ProjectPageHeader = ({ projects, projectId }: ProjectPageProps) => {
+  const project = projects[projectId];
   return (
-    <div className={projectPageHeaderStyles.container}><p>Project page header</p></div>
-  )
-}
+    <div className={projectPageHeaderStyles.container}>
+      <div className={projectPageHeaderStyles.bgContainer}>
+        <div className={projectPageHeaderStyles.bgOverlay}></div>
+        <Image
+          src={projectImg}
+          alt="alt text"
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+        <div className={projectPageHeaderStyles.headerText}>
+          <h2>{project.projectPageTitle}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProjectPageHeader
+export default ProjectPageHeader;
