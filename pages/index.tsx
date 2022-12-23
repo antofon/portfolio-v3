236f29578f2ee2129e1e6b002147c36356efaaf8
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-22 20:01:44 -0800

    Reorganized file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
+import Layout from '../components/Layout';
 import Header from '../components/Header';
 import About from '../components/About';
 import Skills from '../components/Skills/Skills';
 import Projects from '../components/Projects/Projects';
 import Contact from '../components/Contact';
-import Layout from '../components/Layout';
 
 export default function Home() {
   return (
