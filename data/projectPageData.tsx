887aaecbe0cf2f6496fd15f5651476f62e80f9b9
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-22 19:57:24 -0800

    Moved Skills and Project data into separate folder

---
+++ data/projectPageData.tsx
@@ -0,0 +1,67 @@
+export interface ProjectPageProps {
+  projects: ProjectPage[];
+  projectId: number;
+}
+export interface ProjectPage {
+  projectPageHeaderImage: string;
+  projectPageUrl: string;
+  projectDemoUrl: string;
+  projectCodeUrl: string;
+  projectPageTitle: string;
+  projectDescription: string;
+  projectPageStack: string[];
+  projectId: number;
+}
+
+const projectPageData: ProjectPage[] = [
+  {
+    projectPageHeaderImage:
+      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+    projectPageUrl: '/portfolio',
+    projectDemoUrl: 'https://anwanantofon.me/',
+    projectCodeUrl: 'https://github.com/antofon/portfolio-v3',
+    projectPageTitle: 'Portfolio Website',
+    projectId: 0,
+    projectDescription:
+      'This is the third iteration of my Portfolio. It was an exciting challenge for me to apply my Next.js and TypeScript skills to a real-world project.',
+    projectPageStack: ['React.js', 'Next.js', 'TypeScript', 'CSS Modules'],
+  },
+  {
+    projectPageHeaderImage:
+      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+    projectPageUrl: '/stemreads',
+    projectDemoUrl: 'https://stem-reads.netlify.app/',
+    projectCodeUrl: 'https://github.com/antofon/stem-reads',
+    projectPageTitle: 'StemReads',
+    projectDescription:
+      'This is a React web application I created to highlight leaders in STEM. Users can create their own accounts and view a dashboard of selected readings. Upon login, they are able to interact with the dashboard (i.e. delete books, reload to default state, etc).',
+    projectId: 1,
+    projectPageStack: ['React.js', 'Sass', 'Firebase'],
+  },
+  {
+    projectPageHeaderImage:
+      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+    projectPageUrl: '/wikipedia',
+    projectDemoUrl: 'https://antofon.github.io/wikipediaviewer/',
+    projectCodeUrl: 'https://github.com/antofon/wikipediaviewer',
+    projectPageTitle: 'Wikipedia Viewer',
+    projectDescription:
+      'Welcome to the Wikipedia Viewer! This is a legacy project under the freeCodeCamp curriculum for the Front-End Developer Track. This JavaScript app allows users to search for information using the Media Wiki API. Users can also redirect to random articles by clicking the link “View a Random Article” in the app.',
+    projectId: 2,
+    projectPageStack: ['HTML', 'CSS', 'JavaScript', 'Wikipedia API'],
+  },
+  {
+    projectPageHeaderImage:
+      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+    projectPageUrl: '/twitch',
+    projectDemoUrl: 'https://antofon.github.io/twitch-viewer/',
+    projectCodeUrl: 'https://github.com/antofon/twitch-viewer',
+    projectPageTitle: 'Twitch Viewer',
+    projectDescription:
+      'Welcome to the Twitch Viewer! This is a legacy project under the freeCodeCamp curriculum for the Front-End Developer Track. This JavaScript app uses the Twitch API (v5) to display online and offline streamers. The data is present in the script.js file.',
+    projectId: 3,
+    projectPageStack: ['HTML', 'CSS', 'JavaScript', 'Twitch API'],
+  },
+];
+
+export default projectPageData;
---
+++ data/projectsData.tsx
@@ -0,0 +1,42 @@
+export interface ProjectsProps {
+  projects: Projects[];
+}
+export interface Projects {
+  projectsImage: string;
+  projectsTitle: string;
+  projectsStack: string;
+  projectsUrl: string;
+}
+
+const projectsData: Projects[] = [
+  {
+    projectsImage:
+      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+    projectsTitle: 'Portfolio Website',
+    projectsStack: 'React.js / Next.js / TypeScript / CSS Modules',
+    projectsUrl: '/portfolio',
+  },
+  {
+    projectsImage:
+      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+    projectsTitle: 'StemReads',
+    projectsStack: 'React.js / Sass / Firebase',
+    projectsUrl: '/stemreads',
+  },
+  {
+    projectsImage:
+      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+    projectsTitle: 'Wikipedia Viewer',
+    projectsStack: 'HTML / CSS / JavaScript / Wikipedia API',
+    projectsUrl: '/wikipedia',
+  },
+  {
+    projectsImage:
+      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+    projectsTitle: 'Twitch Streamers',
+    projectsStack: 'HTML / CSS / JavaScript / Twitch API',
+    projectsUrl: '/twitch',
+  },
+];
+
+export default projectsData;
