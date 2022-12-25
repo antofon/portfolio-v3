cd392048bc57065acf436694a750c953afe2b9ea
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-25 14:08:38 -0800

    Add footer to main App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import Nav from '../components/Nav';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Nav />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
