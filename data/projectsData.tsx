f6562286212dab6b16d655571ce0d68c9b9caa2d
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-25 14:09:27 -0800

    Updated URLs for project data

--- data/projectsData.tsx
+++ data/projectsData.tsx
@@ -14,28 +14,28 @@ const projectsData: Projects[] = [
       '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
     projectsTitle: 'Portfolio Website',
     projectsStack: 'React.js / Next.js / TypeScript / CSS Modules',
-    projectsUrl: '/portfolio',
+    projectsUrl: '/experience/personal/portfolio',
   },
   {
     projectsImage:
       '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
     projectsTitle: 'StemReads',
     projectsStack: 'React.js / Sass / Firebase',
-    projectsUrl: '/stemreads',
+    projectsUrl: '/experience/personal/stemreads',
   },
   {
     projectsImage:
       '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
     projectsTitle: 'Wikipedia Viewer',
     projectsStack: 'HTML / CSS / JavaScript / Wikipedia API',
-    projectsUrl: '/wikipedia',
+    projectsUrl: '/experience/personal/wikipedia',
   },
   {
     projectsImage:
       '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
     projectsTitle: 'Twitch Viewer',
     projectsStack: 'HTML / CSS / JavaScript / Twitch API',
-    projectsUrl: '/twitch',
+    projectsUrl: '/experience/personal/twitch',
   },
 ];
 
