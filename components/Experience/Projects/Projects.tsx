8b864139bdd425b346878f440ee8c5dab0f62bac
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-25 13:46:05 -0800

    Commented out unused file

---
+++ components/Experience/Projects/Projects.tsx
@@ -0,0 +1,15 @@
+// import projectData from '../../../data/projectsData';
+// import ProjectsList from './ProjectsList';
+// import workData from '../../../data/workData';
+// import WorkList from '../Work/WorkList';
+
+// const Projects = () => {
+//   return (
+//     <>
+//       <WorkList work={workData} />
+//       <ProjectsList projects={projectData} />
+//     </>
+//   );
+// };
+
+// export default Projects;
