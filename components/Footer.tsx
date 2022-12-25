2ed5698518f958c2505ea8baf55b8909e0d21ddb
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-25 14:08:17 -0800

    Different footer styles render on homepage vs experience pages

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,27 @@
 import { useState, useEffect } from 'react';
 import footerStyles from '../styles/Footer.module.css';
 import BackToTop from './BackToTop';
+import { useRouter } from 'next/router';
 
 const Footer = () => {
   const [visible, setVisible] = useState('none');
+  const [homepageFooter, setHomePageFooter] = useState(false)
+const router = useRouter();
+  useEffect(() => {
+    if (
+      // router.asPath === '/experience/personal/portfolio' ||
+      // router.asPath === '/experience/personal/stemreads' ||
+      // router.asPath === '/experience/personal/wikipedia' ||
+      // router.asPath === '/experience/personal/twitch' ||
+      router.asPath === '/experience/work/guru' ||
+      router.asPath === '/experience/work/meta' ||
+      router.asPath === '/experience/work/artdotcom'
+    ) {
+      setHomePageFooter(false);
+    } else {
+      setHomePageFooter(true);
+    }
+  }, [router]);
 
   const handleScroll = () => {
     const bottom =
@@ -27,7 +45,7 @@ const Footer = () => {
     };
   }, []);
   return (
-    <footer className={footerStyles.footer}>
+    <footer className={homepageFooter ? footerStyles.footerHomepage : footerStyles.footerExperience}>
       <BackToTop visible={visible} />
       <p className={footerStyles.footerText}>© 2022 by Anwana Ntofon</p>
     </footer>
