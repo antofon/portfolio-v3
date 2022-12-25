42f1a93c0e70910d3afafe9ecc208cec235f5b6f
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-25 14:07:32 -0800

    Back to top button on renders on homepage

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -1,21 +1,45 @@
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { FaChevronUp } from 'react-icons/fa';
 import backToTopStyles from '../styles/BackToTop.module.css';
+import { useRouter } from 'next/router';
 
 interface BackToTop {
   visible: string;
 }
 
 const BackToTop = ({ visible }: BackToTop) => {
+  const router = useRouter();
+  const [isBackToTop, setIsBackToTop] = useState(true);
+
+  useEffect(() => {
+    if (
+      router.asPath === '/' ||
+      router.asPath === '/#about' ||
+      router.asPath === '/#skills' ||
+      router.asPath === '/#contact' ||
+      router.asPath === '/#about' ||
+      router.asPath === '/#experience'
+    ) {
+      setIsBackToTop(true);
+    } else {
+      setIsBackToTop(false);
+    }
+  }, [router]);
+
   return (
-    <div className={backToTopStyles.container} style={{ display: visible }}>
-      <Link href="/">
-        <div className={backToTopStyles.chevronContainer}>
-          <FaChevronUp />
-          <FaChevronUp className={backToTopStyles.bottomChevron} />
+    <>
+      {isBackToTop ? (
+        <div className={backToTopStyles.container} style={{ display: visible }}>
+          <Link href="/">
+            <div className={backToTopStyles.chevronContainer}>
+              <FaChevronUp />
+              <FaChevronUp className={backToTopStyles.bottomChevron} />
+            </div>
+          </Link>
         </div>
-      </Link>
-    </div>
+      ) : null}
+    </>
   );
 };
 
