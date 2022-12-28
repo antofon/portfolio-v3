383b5b598ed1e4b3c80be9db23c12feb7f05eba1
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-27 21:46:05 -0800

    UpdatedHeader copy and responsiveness

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,18 +4,25 @@ import headerStyles from '../styles/Header.module.css';
 const Header = () => {
   return (
     <div className={headerStyles.container}>
-      <p className={headerStyles.sectionSubtitle}>
-        {"Let's"} build something together
-      </p>
-      <h1 className={headerStyles.sectionTitle}>
+      {/* <p className={headerStyles.sectionSubtitle}>
+        Creative, Web Developer, Musician
+      </p> */}
+      {/* <h1 className={headerStyles.sectionTitle}>
         Hi, {"I'm"} <span className={headerStyles.name}>Anwana</span>
         <br />A Front-End Web Developer
+      </h1> */}
+      
+      <h1 className={headerStyles.sectionTitle}>
+        {/* Hi! {"I'm"} <span className={headerStyles.name}>Anwana</span> */}
+        {/* Oakland-based creative crafting beautiful web experiences focused on <span className={headerStyles.name}>accessibility</span> and <span className={headerStyles.name}>purpose</span> */}
+        Oakland-based web developer devoted to crafting <span className={headerStyles.name}>accessible</span> and <span className={headerStyles.name}>purposeful</span> web experiences. 
       </h1>
 
       <p className={headerStyles.sectionDescription}>
-        {"I'm"} a Front-End Web Developer speicializing in building exceptional
+        {/* {"I'm"} a Front-End Web Developer specializing in building exceptional
         digital experiences. Currently, {"I'm"} focused on building responsive
-        front-end applications while learning back-end technologies.
+        front-end applications while learning back-end technologies. */}
+        
       </p>
 
       <div className={headerStyles.socials}>
