0df118cf9ebec517163cc074de708d367feef813
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-29 00:08:29 -0800

    Updated work data for Experience section Personal tab

--- data/projectsData.tsx
+++ data/projectsData.tsx
@@ -11,28 +11,28 @@ export interface Projects {
 const projectsData: Projects[] = [
   {
     projectsImage:
-      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+      '/assets/images/experience/personal/portfolio/experience_thumbnail_portfolio.png',
     projectsTitle: 'Portfolio Website',
     projectsStack: 'React.js / Next.js / TypeScript / CSS Modules',
     projectsUrl: '/experience/personal/portfolio',
   },
   {
     projectsImage:
-      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+      '/assets/images/experience/personal/stemreads/experience_thumbnail_stemreads.png',
     projectsTitle: 'StemReads',
     projectsStack: 'React.js / Sass / Firebase',
     projectsUrl: '/experience/personal/stemreads',
   },
   {
     projectsImage:
-      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+      '/assets/images/experience/personal/wikipedia/experience_thumbnail_wikipedia.png',
     projectsTitle: 'Wikipedia Viewer',
     projectsStack: 'HTML / CSS / JavaScript / Wikipedia API',
     projectsUrl: '/experience/personal/wikipedia',
   },
   {
     projectsImage:
-      '/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg',
+      '/assets/images/experience/personal/twitch/experience_thumbnail_twitch.png',
     projectsTitle: 'Twitch Viewer',
     projectsStack: 'HTML / CSS / JavaScript / Twitch API',
     projectsUrl: '/experience/personal/twitch',
