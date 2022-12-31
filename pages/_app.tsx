a699120e58dbefc48eb9001d3e166114b649a5b4
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-31 01:05:16 -0800

    Meta/SEO tags added to overall App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,50 @@
-import Nav from '../components/Nav';
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { NextSeo } from 'next-seo';
+import Head from 'next/head';
+import Nav from '../components/Nav';
 import Footer from '../components/Footer';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* meta tags not available in Next SEO library */}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+      </Head>
+      <NextSeo
+        defaultTitle="Anwana Ntofon | Home"
+        additionalMetaTags={[
+          {
+            name: 'keywords',
+            content: 'web development, programming',
+          },
+        ]}
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            href: '/favicon.ico',
+          },
+        ]}
+        description="Oakland-based front-end web developer devoted to crafting accessible and purposeful web experiences."
+        canonical="https://anwanantofon.me"
+        openGraph={{
+          url: 'https://anwanantofon.me',
+          title: 'Anwana Ntofon | Home',
+          description:
+            'Oakland-based front-end web developer devoted to crafting accessible and purposeful web experiences.',
+          images: [
+            {
+              url: '/assets/og-image.png',
+              width: 800,
+              height: 420,
+              alt: 'Anwana Ntofon',
+            },
+          ],
+        }}
+      />
+
       <Nav />
       <Component {...pageProps} />
       <Footer />
