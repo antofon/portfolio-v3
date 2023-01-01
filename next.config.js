b5d6523225afc7b15f1d5b29e4ea952f2e2b206a
Anwana Ntofon <anwanantofon@gmail.com>
2023-01-01 00:30:11 -0800

    Updated config files for successful netlify deploy

--- next.config.js
+++ next.config.js
@@ -2,9 +2,33 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
+  // i18n: {
+  //   locales: ['en'],
+  //   defaultLocale: 'en',
+  // },
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/portfolio': {
+        page: '/portfolio',
+      },
+      '/stemreads': {
+        page: '/stemreads',
+      },
+      '/twitch': { page: '/twitch' },
+      '/wikipedia': {
+        page: '/wikipedia',
+      },
+      '/art': { page: '/art' },
+      '/facebook': { page: '/facebook' },
+      '/guru': { page: '/guru' },
+    };
   },
 };
 
