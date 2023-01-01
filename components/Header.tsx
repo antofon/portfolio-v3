08cfe19dd36a8a1c487b51728d9213d9b697d9a4
Anwana Ntofon <anwanantofon@gmail.com>
2022-12-31 17:03:47 -0800

    Formatting adjustments for Header section

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,27 +11,29 @@ const Header = () => {
         Hi, {"I'm"} <span className={headerStyles.name}>Anwana</span>
         <br />A Front-End Web Developer
       </h1> */}
-      
+
       <h1 className={headerStyles.sectionTitle}>
         {/* Hi! {"I'm"} <span className={headerStyles.name}>Anwana</span> */}
         {/* Oakland-based creative crafting beautiful web experiences focused on <span className={headerStyles.name}>accessibility</span> and <span className={headerStyles.name}>purpose</span> */}
-        Oakland-based web developer devoted to crafting <span className={headerStyles.name}>accessible</span> and <span className={headerStyles.name}>purposeful</span> web experiences. 
+        Oakland-based web developer devoted to crafting{' '}
+        <span className={headerStyles.name}>accessible</span> and{' '}
+        <span className={headerStyles.name}>purposeful</span> web experiences.
       </h1>
 
       <p className={headerStyles.sectionDescription}>
         {/* {"I'm"} a Front-End Web Developer specializing in building exceptional
         digital experiences. Currently, {"I'm"} focused on building responsive
         front-end applications while learning back-end technologies. */}
-        
       </p>
 
       <div className={headerStyles.socials}>
-        <div >
+        <div>
           <a
             href="https://www.linkedin.com/in/anwanantofon/"
             target="_blank"
             rel="noopener noreferrer"
-            className={headerStyles.icon}>
+            className={headerStyles.icon}
+          >
             <FaLinkedinIn />
           </a>
         </div>
@@ -40,12 +42,17 @@ const Header = () => {
             href="https://github.com/antofon"
             target="_blank"
             rel="noopener noreferrer"
-            className={headerStyles.icon}>
+            className={headerStyles.icon}
+          >
             <FaGithub />
           </a>
         </div>
         <div>
-          <a href="https://codepen.io/antofon" rel="noopener noreferrer" className={headerStyles.icon}>
+          <a
+            href="https://codepen.io/antofon"
+            rel="noopener noreferrer"
+            className={headerStyles.icon}
+          >
             <FaCodepen />
           </a>
         </div>
