feb53c6397c284f8b61f138d7327dcf50fba2755
Anwana Ntofon <anwanantofon@gmail.com>
2023-01-01 00:34:02 -0800

    Commented out function to explicitly export pages

--- next.config.js
+++ next.config.js
@@ -9,27 +9,27 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/portfolio': {
-        page: '/portfolio',
-      },
-      '/stemreads': {
-        page: '/stemreads',
-      },
-      '/twitch': { page: '/twitch' },
-      '/wikipedia': {
-        page: '/wikipedia',
-      },
-      '/art': { page: '/art' },
-      '/facebook': { page: '/facebook' },
-      '/guru': { page: '/guru' },
-    };
-  },
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     '/': { page: '/' },
+  //     '/portfolio': {
+  //       page: '/portfolio',
+  //     },
+  //     '/stemreads': {
+  //       page: '/stemreads',
+  //     },
+  //     '/twitch': { page: '/twitch' },
+  //     '/wikipedia': {
+  //       page: '/wikipedia',
+  //     },
+  //     '/art': { page: '/art' },
+  //     '/facebook': { page: '/facebook' },
+  //     '/guru': { page: '/guru' },
+  //   };
+  // },
 };
 
 module.exports = nextConfig;
