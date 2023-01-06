035868b8ebd3fce7cd8d1a77954f9991eaf508e1
Anwana Ntofon <anwanantofon@gmail.com>
2023-01-06 09:24:35 -0800

    Update locale formatting in config file

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
-    //   locales: ['en'],
-    defaultLocale: 'en',
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
   },
   images: {
     unoptimized: true,
