4f3c7c6cd536a6b6953f585558ba6e833ce69817
Anwana Ntofon <anwanantofon@gmail.com>
2023-01-08 16:38:45 -0800

    Code cleanup for Work page component

--- components/WorkPage/WorkPageBody.tsx
+++ components/WorkPage/WorkPageBody.tsx
@@ -1,31 +1,27 @@
-import workPageBodyStyles from '../../styles/WorkPage/WorkPageBody.module.css';
-import { WorkPageProps } from '../../data/workPageData';
-import { FaDotCircle } from 'react-icons/fa';
-import { useRouter } from 'next/router';
 import Link from 'next/link';
-import ImageCarousel from '../ImageCarousel';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { WorkPageProps } from '../../data/workPageData';
+import workPageBodyStyles from '../../styles/WorkPage/WorkPageBody.module.css';
 
 const WorkPageBody = ({ work, workId }: WorkPageProps) => {
   const workItem = work[workId];
   const router = useRouter();
   return (
     <div className={workPageBodyStyles.projectBody}>
-      <div className={workPageBodyStyles.projectBodyText}>
-        {/* <p className={workPageBodyStyles.bodyProjectTitle}>Project</p> */}
-        {/* <h2 className={workPageBodyStyles.bodyOverviewTitle}>Overview</h2> */}
+      <div>
         <h2 className={workPageBodyStyles.bodyOverviewTitle}>Gallery</h2>
         <p className={workPageBodyStyles.projectDescription}>
-          {workItem.workPageDescription} <a
-          href={workItem.url}
-          rel="nofollow noreferrer"
-          target="_blank"
-         style={{textDecoration:"underline"}}>
-          Website
-        </a>
+          {workItem.workPageDescription}{' '}
+          <a
+            href={workItem.url}
+            rel="nofollow noreferrer"
+            target="_blank"
+            style={{ textDecoration: 'underline' }}
+          >
+            Website
+          </a>
         </p>
-        
-
         <div className={workPageBodyStyles.imageContainer}>
           {workItem.workPageGallery.map((item, index) => (
             <Image
@@ -54,11 +50,6 @@ const WorkPageBody = ({ work, workId }: WorkPageProps) => {
             Website
           </a>
         </div> */}
-        {/* {router.asPath === '/experience' ? null : (
-          <Link href="/#experience" className={workPageBodyStyles.backButton}>
-            Back
-          </Link>
-        )} */}
       </div>
     </div>
   );
--- components/WorkPage/WorkPageHeader.tsx
+++ components/WorkPage/WorkPageHeader.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import Image from 'next/image';
 import { WorkPageProps } from '../../data/workPageData';
 import workPageHeaderStyles from '../../styles/WorkPage/WorkPageHeader.module.css';
-import workImg from '../../public/assets/images/about/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpeg';
 
 const WorkPageHeader = ({ work, workId }: WorkPageProps) => {
   const workItem = work[workId];
@@ -14,11 +12,24 @@ const WorkPageHeader = ({ work, workId }: WorkPageProps) => {
           src={workItem.workPageHeaderImage}
           alt="alt text"
           fill
-          style={{ objectFit: 'cover', filter:"blur(8px)" }}
+          style={{ objectFit: 'cover', filter: 'blur(8px)' }}
         />
-        <div className={workId === 1 ? `${workPageHeaderStyles.headerMetaText}` : `${workPageHeaderStyles.headerText}`}>
-          <h2 className={workPageHeaderStyles.titleText}>{workItem.workPageTitle}</h2>
-          <p className={workPageHeaderStyles.positionText}style={{color: "white"}}>{workItem.workPagePosition}</p>
+        <div
+          className={
+            workId === 1
+              ? `${workPageHeaderStyles.headerMetaText}`
+              : `${workPageHeaderStyles.headerText}`
+          }
+        >
+          <h2 className={workPageHeaderStyles.titleText}>
+            {workItem.workPageTitle}
+          </h2>
+          <p
+            className={workPageHeaderStyles.positionText}
+            style={{ color: 'white' }}
+          >
+            {workItem.workPagePosition}
+          </p>
         </div>
       </div>
     </div>
